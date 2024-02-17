9515e6867b6b4cb764c1109bc3705ad0be077f42
HiteshwarKaushik <thehiteshwarkaushik@gmail.com>
2024-02-17 22:39:19 +0530

    Added gallery carousel (nf), Added info section on home page (nf)

--- client/src/Pages/Gallery.jsx
+++ client/src/Pages/Gallery.jsx
@@ -1,13 +1,15 @@
 import Navbar from '../Components/Navbar'
 import Footer from '../Components/Footer'
+import GalleryCarousel from '../Components/GalleryCarousel'
 
 const Gallery = () => {
-  const gridData = ['Data 1', 'Data 2', 'Data 3', 'Data 4', 'Data 5', 'Data 6'];
   return (
     <div>
       <Navbar />
       <div className='w-full h-[45rem] relative border border-black flex justify-center items-center'>
-        <div className='w-[90%] h-[75%] border border-black'></div>
+        <div className='w-[90%] h-[75%] border border-black'>
+          <GalleryCarousel />
+        </div>
       </div>
       <Footer />
     </div>
