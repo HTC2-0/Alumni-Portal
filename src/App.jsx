d8fe3d0669c7a1e467e6b5ec0c95da11e6d85567
HiteshwarKaushik <thehiteshwarkaushik@gmail.com>
2024-02-17 14:17:26 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Home from "./Pages/Home.jsx"
+import Alumni from "./Pages/Alumni.jsx"
+import Gallary from "./Pages/Gallary.jsx"
+import Events from "./Pages/Events.jsx"
+import SignUP from "./Pages/SignUp.jsx"
+import SignIn from "./Pages/SignIn.jsx"
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <Route path="/home" element={<Home />} />
+      <Route path="/alumni" element={<Alumni />} />
+      <Route path="/gallary" element={<Gallary />} />
+      <Route path="/events" element={<Events />} />
+      <Route path="/signup" element={<SignUP />} />
+      <Route path="/signin" element={<SignIn />} />
+    </div>
+  )
+}
+
+export default App
+
