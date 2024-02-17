a76f84eb1cc6203b915fabdcad4dcfe7af65aa4a
Hiteshwar Kaushik <125125243+CoderKaushik@users.noreply.github.com>
2024-02-17 19:50:34 +0530

    Update Home.jsx, added register and login routes (temporarily)

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,8 @@
 import Navbar from "../Components/Navbar";
 import { Link } from "react-router-dom";
 import Footer from "../Components/Footer";
+import Register from "./Register.jsx';
+import SignIn from "./SignIn.jsx";
 import HomeCarousel from "../Components/HomeCarousel";
 
 const Home = () => {
@@ -12,6 +14,8 @@ const Home = () => {
       <Link to="/events">Events</Link>
       <Link to="/gallary">Gallery</Link>
       <Link to="/profile">Profile</Link>
+      <Link to="/register">Register</Link>
+      <Link to="/login">Login</Link>
       <Footer />
     </div>
   );
