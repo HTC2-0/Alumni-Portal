95e6f571235afc13af9fb09832cc142ca1f1f330
Hiteshwar Kaushik <125125243+CoderKaushik@users.noreply.github.com>
2024-02-17 16:19:15 +0530

    Update Home.jsx, added links for Gallery, Profile, Alumni and Events

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import Navbar from '../Components/Navbar';
+import { Link } from 'react-router-dom'
 import Footer from '../Components/Footer';
 import HomeCarousel from '../Components/HomeCarousel';
 import React from 'react';
@@ -8,6 +9,10 @@ const Home = () => {
     <div>
       <Navbar />
       <HomeCarousel />
+      <Link to="/alumni">Alumni</Link>
+      <Link to="/events">Events</Link>
+      <Link to="/gallary">Gallery</Link>
+      <Link to="/profile">Profile</Link>
       <Footer /> 
     </div>
   );
